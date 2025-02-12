2ae6c8df8fa39e0523fccb9d54e5dcf90d39f35c
Carlos Gonzalez <uno1425@gmail.com>
2025-02-12 13:56:56 -0600

    Building responsive UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-import { Grid } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-  return <Grid templateAreas={`"nav nav" "aside main"`}></Grid>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`, //1024px
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <GridItem display={{ base: "none", lg: "block" }} area="aside" bg="gold">
+        Aside
+      </GridItem>
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
