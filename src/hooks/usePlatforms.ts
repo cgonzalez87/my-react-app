e9e2b002ba885ad8ded4a62fd670321554e2e9d6
Carlos Gonzalez <uno1425@gmail.com>
2025-03-04 11:53:39 -0600

    Upgradin fetching platforms method to use ReactQuery

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,4 +1,7 @@
-import useData from "./useData";
+import { useQuery } from "@tanstack/react-query";
+import useData, { FetchResponse } from "./useData";
+import apiClient from "../services/api-client";
+import { Platform } from "./useGames";
 
 interface Plaforms {
     id: number;
@@ -6,5 +9,10 @@ interface Plaforms {
     slug: string;
 }
 
-const usePlatforms = () => useData<Plaforms>("/platforms/lists/parents")
+const usePlatforms = () => useQuery({
+    queryKey: ['platforms'],
+    queryFn: () => apiClient.get<FetchResponse<Platform>>('/platforms').then(res => res.data),
+    staleTime: 24 * 60 * 60 * 1000, // 24h      
+    // initialData: { count: 0, results: [] as Plaforms[] }
+})
 export default usePlatforms;
